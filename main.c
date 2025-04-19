ad53e0efa4eb43b55482bb6c9faf732957749fd5
Gomes <joaovitorg.silva050@gmail.com>
2025-04-19 00:53:11 -0300

    feat: comentando o codigo. task: #10

--- main.c
+++ main.c
@@ -31,31 +31,33 @@ int main(int argc, char *argv[]){
         sprintf(buffer, "Execute no formato: %s <id do roteador>", argv[0]);
         die(buffer);
     }
+    // Registra o roteador ativo
     nucleo.id = atoi(argv[1]);
     
     FILE *f;
     if((f = fopen("enlaces.config", "r")) == NULL) die("Erro ao abrir enlaces.config");
-    readEnlaces(0, f);
-    fclose(f);
+    readEnlaces(0, f); // Lê os enlaces. 
+    fclose(f); // Fecha o arquivo.
 
-    qsort(roteadores, nucleo.qtdVizinhos, sizeof(Roteador), cmp);
+    // Ordena os roteadores pelo id.
+    qsort(roteadores, nucleo.qtdVizinhos, sizeof(Roteador), cmp); 
 
     if((f = fopen("roteador.config", "r")) == NULL) die("Erro ao abrir roteador.config");
-    readRoteadores(nucleo.qtdVizinhos, f);
-    fclose(f);
+    readRoteadores(f); // Salva os endereços dos roteadores.
+    fclose(f); // Fecha o arquivo.
 
-    struct sockaddr_in si_me;
-    int sock = makeSocket(si_me, nucleo.endereco.porta);
+    // Cria o socket configurado.
+    int sock = makeSocket(nucleo.endereco.porta);
 
-    // para o prinft n se cagar inteiro
-    setvbuf(stdout, 0, _IONBF, 0) ;
+    // Cria 1 thread para cada serviço.
+    pthread_t t_receiver, t_sender, t_queue_sender;
 
-    pthread_t t_receiver, t_sender;
-
+    // Seta as respectivas funções 
     pthread_create(&t_receiver, NULL, receiver, &sock);
     pthread_create(&t_sender, NULL, sender, &sock);
 
-    pthread_join(t_sender, NULL);    // sender decide se o programa encerra 
+    // O programa espera até a thread do sender encerrar 
+    pthread_join(t_sender, NULL);    
     
     close(sock);
     return 0;
@@ -64,43 +66,56 @@ int main(int argc, char *argv[]){
 void *sender(void *socket) {
     struct sockaddr_in si_other;
 
+    // Zera a struct do endereço de destino
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
+
+    // Recupera o descritor de socket passado como argumento
     int s = *(int *)socket;
+
     Mensagem msg;
-   
-    msg.fonte = nucleo.endereco;
+    msg.fonte = nucleo.endereco; // Define o endereço de origem da mensagem como o núcleo
+
     int id, op;
-    char buffer[BUFLEN + 3];
+    char buffer[BUFLEN + 3]; // Buffer para entrada de usuário (id + espaço + mensagem
 
     while (1){
         printf("+--------------------------------------+\n");
-        printf("| %-37s|\n", "Escolha uma opção");
+        printf("| %-39s|\n", "Escolha uma opção");
         printf("| %-37s|\n", "0 - Digitar uma mensagem");
         printf("| %-37s|\n", "1 - Sair");
         printf("+--------------------------------------+\n");
 
+        // Lê a opção do usuário
         scanf("%d", &op);
-        if(op) return NULL;
+        if (op) return NULL;  // Se for 1, encerra a thread
         
-        while (getchar() != '\n'); // limpa o buffer
+        // Remove os resíduos do buffer de entrada
+        while (getchar() != '\n'); 
 
         printf("Informe a mensagem no formato: <id> <msg>\n");
-        fflush(stdout);    
+        fflush(stdout);  // Garante que a mensagem seja impressa imediatamente
+
+        // Lê a linha completa do stdin
         fgets(buffer, BUFLEN + 3, stdin);
+
+        // Tenta extrair o id e a mensagem da linha
         if (sscanf(buffer, "%d %[^\n]", &id, msg.data) != 2) {
             perror("Formato inválido. Use: <id> <mensagem>\n");
             continue;
         }
 
-        msg.tipoMensagem = DADOS;
-
+        // Procura o roteador informado
         Roteador *r = findById(id);
         if(!r) printf("Roteador não existe ou não é vizinho");
 
+        memset(&msg, 0, sizeof(Mensagem)); // reset na var antes do uso
+        // Seta o tipo da mensagem e o endereço do destino
+        msg.tipoMensagem = DADOS;
+
         msg.destino = r->endereco;
         si_other.sin_port = htons(msg.destino.porta);
-        
+        // Envia a mensagem para o roteador
         if (sendto(s, &msg, sizeof(msg), 0 ,(struct sockaddr *) &si_other, sizeof(si_other))==-1) die("sendto()");
         
     }
--- utils.c
+++ utils.c
@@ -12,7 +12,8 @@ void die(char *s)
     exit(1);
 }
 
-int makeSocket(struct sockaddr_in si_me, int port){
+int makeSocket(int port){
+    struct sockaddr_in si_me;
     int s;
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) die("socket");
     
@@ -30,42 +31,43 @@ int makeSocket(struct sockaddr_in si_me, int port){
 
 void readEnlaces(int i, FILE *file){
     int roteador1, roteador2, enlace;
-    // para entender melhor só descomentar esses printf
-    // 2 x (2 char id) + 4 char enlace + 2 char espaço + 1 \n
+    // 2 x (2 char id) + 4 char enlace + 2 char espaço + 1 (\0 | \n) = 11.
     char linha[11];
 
     do {
-    
+        // lê os bytes linha do arquivo e verifica o caso base da recursão.
         if(!fgets(linha, 11, file)){ 
-            // printf("\nagr to alocando ");
-
-            // printf("%d \n",i);
-            // se chegou aqui significa q lemos todo o arquivo, alocamos espaço para todos roteadores vizinhos 
+            // Agora que se sabe a quantidades de roteadores, aloca memória.
             roteadores = (Roteador *)malloc(sizeof(Roteador) * i);
-            // finaliza a recursão 
+            // salva essa quantidade no roteador main encerrando a recursão. 
             nucleo.qtdVizinhos = i;
             return ;
         };
-        // separa a linha lida 
+        // Trata os bytes lidos para o formato esperado.
         sscanf(linha, "%d %d %d", &roteador1, &roteador2, &enlace);
-        // printf("l: %s",linha);
+        // verifica se o enlace lido é um enlace do roteador main.
     } while (roteador1 != nucleo.id && roteador2 != nucleo.id );
     
-    // se o roteador atual faz parte do enlace ele para aqui e chama a função +1
+    // Chamada da recursão adicionando incrementando o numero de roteadores encontrados.
     readEnlaces(i + 1, file);
-    // printf("es: %d %d %d\n", roteador1, roteador2, enlace);
+    // Salva o id do roteador encontrado
+    // considerando que o roteador1 ou roteador são o id do nucleo 
+    // temos x ^ x ^ y e sabe-se que x ^ x = 0 e 0 ^ y = y.
     roteadores[i].id = roteador1 ^ roteador2 ^ nucleo.id ;
     roteadores[i].enlace = enlace;
 }
 
-void readRoteadores(int max, FILE *file){
-    int hit = max;
+void readRoteadores(FILE *file){
+    // Variável de parada.
+    int hit = nucleo.qtdVizinhos;
     int id, porta;
-    // 2 char id + 6 char porta + 15 char ip + 2 char espaco + 1 \n
+    // 2 char id + 6 char porta + 15 char ip + 2 char espaco + 1 \n.
     char linha[26], ip[16];
-    while(fgets(linha, 26, file) && hit){
+    while(hit){
+        fgets(linha, 26, file);
         sscanf(linha, "%d %d %s", &id, &porta, ip);
 
+        // Se for o próprio núcleo, atualiza o endereço
         if(nucleo.id == id) {
             strncpy(nucleo.endereco.ip, ip, 16);
             nucleo.endereco.porta = porta;
@@ -74,18 +76,19 @@ void readRoteadores(int max, FILE *file){
         Roteador *r = findById(id);
         if(!r) continue;
 
+        // Atualiza o roteador encontrado        
         strncpy(r->endereco.ip, ip, 16);
         r->endereco.porta = porta;
         hit--;
-        break;
     }
 }
 
-inline int cmp(const void *a, const void *b) {
+int cmp(const void *a, const void *b) {
     return ((Roteador *)a)->id - ((Roteador *)b)->id;
 }
 
 Roteador *findById(int id) {
+    // Cria um roteador temporário com id e faz uma busca binária
     Roteador chave = {.id = id};
     return bsearch(&chave, roteadores, nucleo.qtdVizinhos, sizeof(Roteador), cmp);
 }
